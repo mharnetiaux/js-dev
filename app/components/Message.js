f74028b184c56812a1f09f596265a48ae41d8b35
mharnetiaux <mattskyh@gmail.com>
2019-08-16 03:46:17 -0700

    update to comments.

--- app/components/Message.js
+++ app/components/Message.js
@@ -4,7 +4,7 @@ import LikeButton from '../components/LikeButton.js';
 const Message = (props) => {
     const chatContent = Object.keys(props.messages).map((item) => props.messages[item]);
     return(
-        <div>
+        <span>
             {chatContent.map((content, id) =>
                 <ul key={id} className="message">
                     <li className="user">{content.user}</li>
@@ -12,7 +12,7 @@ const Message = (props) => {
                     <li><LikeButton likeCount={content.likes} /></li>
                 </ul>
             )}
-        </div>
+        </span>
     );
 };
 export default Message;
